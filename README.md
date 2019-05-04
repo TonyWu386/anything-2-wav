616e8877e5387e8e29390f5f5caf2a8e2cc43e7d
TonyWu386 <xb.wu@mail.utoronto.ca>
2019-05-04 19:16:24 -0400

    Add bit depth option and defaults

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 # anything-2-wav
 This tool converts files of any type into valid and playable wav audio files
 
-Copyright (c) 2018 [Tony Wu], All Right Reserved
+Copyright (c) 2018 [Tony Wu], All Rights Reserved
 
 License: GNU GPL v3.0
+
+Requires [PyCryptodome](https://pypi.org/project/pycryptodome/)
 
 Features:
 
@@ -11,7 +13,7 @@ Features:
 
 - Adjustable bitrate target (affects playback duration)
 
-- Adjustable number of audio channels
+- Adjustable number of audio channels and bit depth
 
 All output wav files sound like static. This is intentional, and indicates the encryption is working. Authenticated encryption (AEAD) is provided by ChaCha20-Poly1305 from the pycryptodome library. Key derivation is provided by Scrypt. This system prevents extraction of the original file without the correct passcode/keyfile.
 
@@ -19,18 +21,18 @@ All output wav files sound like static. This is intentional, and indicates the e
 sh-4.4$ file img_file.png
 img_file.png: PNG image data, 1241 x 1241, 8-bit/color RGBA, non-interlaced
 
-sh-4.4$ python3 anything2wav.py -e -r 256 -c 2 -o legit_wav_file.wav img_file.png
+sh-4.4$ python3 anything2wav.py -e -r 256 -c 2 -b 16 -o legit_wav_file.wav img_file.png
 no keyfile provided - asking for passcode instead
 enter passcode:
 
 sh-4.4$ file legit_wav_file.wav
-legit_wav_file.wav: RIFF (little-endian) data, WAVE audio, Microsoft PCM, 8 bit, stereo 16036 Hz
+legit_wav_file.wav: RIFF (little-endian) data, WAVE audio, Microsoft PCM, 16 bit, stereo 8018 Hz
 
 sh-4.4$ ffprobe legit_wav_file.wav
 ...
 Input #0, wav, from 'legit_wav_file.wav':
   Duration: 00:00:52.93, bitrate: 256 kb/s
-    Stream #0:0: Audio: pcm_u8 ([1][0][0][0] / 0x0001), 16036 Hz, 2 channels, u8, 256 kb/s
+    Stream #0:0: Audio: pcm_s16le ([1][0][0][0] / 0x0001), 8018 Hz, 2 channels, u16, 256 kb/s
 
 sh-4.4$ python3 anything2wav.py -d -o back_again.png legit_wav_file.wav
 no keyfile provided - asking for passcode instead
--- src/anything2wav.py
+++ src/anything2wav.py
@@ -15,9 +15,14 @@ from Crypto.Protocol.KDF import scrypt
 
 from getpass import getpass
 from collections import namedtuple
+from pathlib import Path
 import wave
 
 
+# Constants
+DEFAULT_BITRATE = 128
+DEFAULT_CHANNELS = 2
+DEFAULT_BITDEPTH = 16
 SCRYPT_ITER = 131072
 
 
@@ -33,8 +38,8 @@ def getCrypter(keyRaw, kdfSalt, nonce):
     return ChaCha20_Poly1305.new(key = keyDerived, nonce = nonce)
 
 
-def encode(keyRaw, inputFile, outputFile, bitrate, channels):
-    ''' (bytes, str, str, float, int) -> None
+def encode(keyRaw, inputFile, outputFile, bitrate, channels, bitDepth):
+    ''' (bytes, str, str, float, int, int) -> None
     Encodes the inputFile into a wav file based on parameters
     '''
 
@@ -52,15 +57,16 @@ def encode(keyRaw, inputFile, outputFile, bitrate, channels):
     wp = namedtuple('_wave_params', ['nchannels', 'sampwidth', 'framerate', \
     'nframes', 'comptype', 'compname'])
     nframes = 1 + len(kdfSalt + nonce_rfc7539 + authData + cipherbytes)
-    framerate = (125.284 * bitrate) / channels
-    header = wp(channels, 1, framerate, nframes, 'NONE', 'not compressed')
+    rounding = channels * bitDepth
+    framerate = (125.284 * bitrate) / rounding
+    header = wp(channels, bitDepth, framerate, nframes, 'NONE', 'not compressed')
 
     # The WAV format truncates frame data to multiples of the channel quantity
-    # Padding may be required in this case
+    # and audio bit depth - Padding may be required in this case
     paddingCount = 0
-    if (channels > 1):
-        if (nframes % channels > 0):
-            paddingCount = channels - (nframes % channels)
+    if (rounding > 1):
+        if (nframes % rounding > 0):
+            paddingCount = rounding - (nframes % rounding)
 
     # Write new wav file to disk
     with wave.open(outputFile, 'wb') as w:
@@ -128,15 +134,17 @@ if __name__ == "__main__":
         print ('Options:')
         print ('\t -e, --encode')
         print ('\t -d, --decode')
-        print ('\t -r, --bitrate')
-        print ('\t -c, --channels')
+        print ('\t -r, --bitrate FLOAT')
+        print ('\t -c, --channels INT')
+        print ('\t -b, --bitdepth 8 | 16 | 32 | 64')
         print ('\t -k key_file, --key=key_file')
         print ('\t -o output_file, --output=output_file')
         sys.exit(2)
 
     try:
-        opts, args = getopt.getopt(sys.argv[1:], "hedr:c:k:o:", ["help", \
-        "encode", "decode", "bitrate=", "channels=", "key=", "output="])
+        opts, args = getopt.getopt(sys.argv[1:], "hedr:c:b:k:o:", ["help", \
+        "encode", "decode", "bitrate=", "channels=", "bitdepth=", "key=", \
+        "output="])
     except getopt.GetoptError as err:
         print(err)
         usage()
@@ -147,6 +155,7 @@ if __name__ == "__main__":
     keyFile = None
     outputFile = None
     channels = None
+    bitDepth = None
     inputFile = args[0] if len(args) > 0 else None
     for opt, arg in opts:
         if opt in ("-h", "--help"):
@@ -159,6 +168,8 @@ if __name__ == "__main__":
             bitrate = arg
         elif opt in ("-c", "--channels"):
             channels = arg
+        elif opt in ("-b", "--bitdepth"):
+            bitDepth = arg
         elif opt in ("-k", "--key"):
             keyFile = arg
         elif opt in ("-o", "--output"):
@@ -169,36 +180,52 @@ if __name__ == "__main__":
         print('encode/decode option is missing\n')
         usage()
 
-    if (bitrate is None and mode is "encode"):
-        print('bitrate option is missing and mode is encode\n')
-        usage()
-    if (bitrate is not None and mode is "decode"):
-        print('bitrate option is present and mode is decode\n')
-        usage()
-    if (bitrate is not None):
-        if (not bitrate.replace('.','',1).isdigit()):
+    if (mode is "encode"):
+        if (bitrate is None):
+            print('bitrate option is missing - defaulting to {}'.format(DEFAULT_BITRATE))
+            bitrate = DEFAULT_BITRATE
+        elif (not bitrate.replace('.','',1).isdigit()):
             print('bitrate must be a positive number\n')
             usage()
         else:
             bitrate = float(bitrate)
+    elif (bitrate is not None):
+        print('bitrate option is present and mode is decode\n')
+        usage()
 
-    if (channels is None and mode is "encode"):
-        print('channel option is missing and mode is encode\n')
-        usage()
-    if (channels is not None and mode is "decode"):
-        print('channel option is present and mode is decode\n')
-        usage()
-    if (channels is not None):
-        if (not channels.isdigit()):
+    if (mode is "encode"):
+        if (channels is None):
+            print('channel option is missing - defaulting to {}'.format(DEFAULT_CHANNELS))
+            channels = DEFAULT_CHANNELS
+        elif (not channels.isdigit()):
             print('channels must be a positive integer\n')
             usage()
         else:
             channels = int(channels)
+    else:
+        if (channels is not None):
+            print('channel option is present and mode is decode\n')
+            usage()
+
+    if (mode is "encode"):
+        if (bitDepth is None):
+            print('bitdepth option is missing - defaulting to {}-bit'.format(DEFAULT_BITDEPTH))
+            bitDepth = DEFAULT_BITDEPTH
+        elif (int(bitDepth) not in [8, 16, 32, 64]):
+            print('bitdepth option has an invalid value\n')
+            usage()
+        bitDepth = int(bitDepth)//8
+    elif (bitDepth is not None):
+        print('bitdepth option is present and mode is decode\n')
+        usage()
 
     if (keyFile is None):
         print('no keyfile provided - asking for passcode instead')
         keyRaw = getpass(prompt = "enter passcode: ").encode()
     else:
+        if (not Path(keyFile).is_file()):
+            print('cannot find specified keyfile\n')
+            sys.exit(2)
         with open(keyFile, 'rb') as f:
             keyRaw = f.read()
 
@@ -209,7 +236,11 @@ if __name__ == "__main__":
         print('input_file is missing\n')
         usage()
 
+    if (not Path(inputFile).is_file()):
+        print('cannot find specified input file\n')
+        sys.exit(2)
+
     if (mode is "encode"):
-        encode(keyRaw, inputFile, outputFile, bitrate, channels)
+        encode(keyRaw, inputFile, outputFile, bitrate, channels, bitDepth)
     else:
         decode(keyRaw, inputFile, outputFile)
